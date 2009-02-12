fff564e5615686c0c78ccc370e15dc8f69e2a82a
airizar <airizar@e6c00612-ca82-11dd-a8b6-f5aca7b582bb>
2009-02-12 12:54:12 +0000

    Initial import.

---
+++ trunk/ejemplo7/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3g.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" lang="es">
+	<head>
+		<title>Mi pagina</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<link href="estilo.css" rel="stylesheet" type="text/css" />
+	</head>
+	<body>
+		<?php
+		$cad="hola-que-tal?";
+		$cad1=str_replace("-","_",$cad);
+		print_r($cad1);
+		//echo str_replace("world","Peter","Hello world!");
+
+		?>
+	</body>
+</html>
